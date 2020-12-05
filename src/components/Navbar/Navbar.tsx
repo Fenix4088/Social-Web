2920b28b9b5ef94422089483201452e0b8ad61b0
Yehor Pliasov <fenix4088@gmail.com>
2020-12-05 18:48:58 +0100

    Created css modules for first three components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import classes from './Navbar.module.css'
+
+const Navbar = () => {
+  console.log(classes);
+  return  (
+    <nav className={classes.nav}>
+      <div className={`${classes.item} ${classes.active}`}>
+        Profile
+      </div>
+      <div className={classes.item}>
+        Messages
+      </div>
+      <div className={classes.item}>
+        News
+      </div>
+      <div className={classes.item}>
+        Music
+      </div>
+      <div className={classes.item}>
+        Settings
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import classes from './Profile.module.css'
+
 
 const Profile = () => {
+
   return  (
-    <main className="content">
+    <main className={classes.content}>
       <div>
         <img src="https://cdn.cnn.com/cnnnext/dam/assets/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg"
              alt="Background personal" />
@@ -14,9 +17,9 @@ const Profile = () => {
         My posts
         <div>New post</div>
         <div>
-          <div>post 1</div>
-          <div>post 2</div>
-          <div>post 3</div>
+          <div className={classes.item}>post 1</div>
+          <div className={classes.item}>post 2</div>
+          <div className={classes.item}>post 3</div>
         </div>
       </div>
     </main>
