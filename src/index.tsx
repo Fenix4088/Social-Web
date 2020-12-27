4dae7598c472d4b3f62dfb01853cb9ed7d702c52
Yehor Pliasov <fenix4088@gmail.com>
2020-12-27 19:20:35 +0100

    37 Created face store

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import {addPost, state, stateType, subscribe, updateNewPostText} from './redux/state';
+import {store, stateType} from './redux/state';
 import React from 'react';
+
 
  const rerenderEntireTree = (state: stateType): void => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>,
         document.getElementById('root')
     );
 }
 
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,120 +1,123 @@
-let rerenderEntireTree = (state: stateType) => {
-  console.log("State render")
-}
 
 export type stateType = {
-  profilePageData: {
-    postsData: Array<PostsDataType>
-    newPostText: string
-  }
-  dialogsPageData: {
-    dialogsData: Array<DialogItemType>
-    messagesData: Array<MessageDataType>
-  }
-  sidebar: {
-    friends: Array<FriendsItemType>
-  }
+    profilePageData: {
+        postsData: Array<PostsDataType>
+        newPostText: string
+    }
+    dialogsPageData: {
+        dialogsData: Array<DialogItemType>
+        messagesData: Array<MessageDataType>
+    }
+    sidebar: {
+        friends: Array<FriendsItemType>
+    }
 }
 export type PostsDataType = {
-  id: number
-  message: string
-  likes: number
+    id: number
+    message: string
+    likes: number
 }
 export type MessageDataType = {
-  id: number
-  message: string
+    id: number
+    message: string
 }
 export type DialogItemType = {
-  name: string
-  id: number
-  logo: string
+    name: string
+    id: number
+    logo: string
 }
 export type FriendsItemType = {
-  id: number
-  name: string
-  logo: string
+    id: number
+    name: string
+    logo: string
 }
 
-//! State
-export const state: stateType = {
-  profilePageData: {
-    postsData: [
-      { id: 1, message: 'Good day', likes: 15 },
-      { id: 2, message: 'Nice weather', likes: 6 },
-      { id: 3, message: 'I was in Rome!!!!', likes: 0 }
-    ],
-    newPostText: ""
-  },
-  dialogsPageData: {
-    messagesData: [
-      { id: 1, message: 'Hi' }, { id: 2, message: 'How are you?' }, { id: 3, message: 'Where are you from?' },
-      { id: 4, message: 'Nice picture? bro!!!!' }, { id: 5, message: 'I miss you :-(' }
-    ],
-    dialogsData: [
-      {
-        id: 1,
-        name: 'Yehor',
-        logo: 'https://c0.klipartz.com/pngpicture/795/938/gratis-png-cerdo-blanco-grande-caras-divertidas-de-dibujos-animados-s.png'
-      },
-      {
-        id: 2,
-        name: 'Andrey',
-        logo: 'https://e7.pngegg.com/pngimages/95/869/png-clipart-walrus-santa-claus-drawing-funny-cartoon-faces-s-food-face.png'
-      },
-      {
-        id: 3,
-        name: 'Viktor',
-        logo: 'https://w7.pngwing.com/pngs/2/519/png-transparent-little-penguin-bird-face-funny-cartoon-faces-s-face-smiley-bird.png'
-      },
-      { id: 4, name: 'Dima', logo: 'https://wallpaperaccess.com/full/777162.png' },
-      {
-        id: 5,
-        name: 'Sveta',
-        logo: 'https://funnypicture.org/wallpaper/2015/05/funny-cartoon-faces-28-high-resolution-wallpaper.png'
-      }
-    ]
-  },
-  sidebar: {
-    friends: [
-      {
-        id: 1,
-        name: 'Sveta',
-        logo: 'https://c0.klipartz.com/pngpicture/795/938/gratis-png-cerdo-blanco-grande-caras-divertidas-de-dibujos-animados-s.png'
-      },
-      {
-        id: 2,
-        name: 'Anna',
-        logo: 'https://w7.pngwing.com/pngs/2/519/png-transparent-little-penguin-bird-face-funny-cartoon-faces-s-face-smiley-bird.png'
-      },
-      {
-        id: 3,
-        name: 'Dima',
-        logo: 'https://funnypicture.org/wallpaper/2015/05/funny-cartoon-faces-28-high-resolution-wallpaper.png'
-      }
-    ]
-  }
-};
-//! Functions for changing State
-export const addPost = (): void => {
-  const newPost = {
-    id: 4,
-    message: state.profilePageData.newPostText,
-    likes: 0
-  };
-  state.profilePageData.postsData.push(newPost);
-  state.profilePageData.newPostText = "";
-  rerenderEntireTree(state);
-};
-export const updateNewPostText = (newText: string): void => {
-  const newPost = {
-    id: 4,
-    message: postMessage,
-    likes: 0
-  };
-  state.profilePageData.newPostText = newText;
-  rerenderEntireTree(state);
-};
-export const subscribe = (observer: (state: stateType) => void) => {
-  rerenderEntireTree = observer;
-}
+//! Store
+export const store = {
+    _state: {
+        profilePageData: {
+            postsData: [
+                { id: 1, message: 'Good day', likes: 15 },
+                { id: 2, message: 'Nice weather', likes: 6 },
+                { id: 3, message: 'I was in Rome!!!!', likes: 0 }
+            ],
+            newPostText: ""
+        },
+        dialogsPageData: {
+            messagesData: [
+                { id: 1, message: 'Hi' }, { id: 2, message: 'How are you?' }, { id: 3, message: 'Where are you from?' },
+                { id: 4, message: 'Nice picture? bro!!!!' }, { id: 5, message: 'I miss you :-(' }
+            ],
+            dialogsData: [
+                {
+                    id: 1,
+                    name: 'Yehor',
+                    logo: 'https://c0.klipartz.com/pngpicture/795/938/gratis-png-cerdo-blanco-grande-caras-divertidas-de-dibujos-animados-s.png'
+                },
+                {
+                    id: 2,
+                    name: 'Andrey',
+                    logo: 'https://e7.pngegg.com/pngimages/95/869/png-clipart-walrus-santa-claus-drawing-funny-cartoon-faces-s-food-face.png'
+                },
+                {
+                    id: 3,
+                    name: 'Viktor',
+                    logo: 'https://w7.pngwing.com/pngs/2/519/png-transparent-little-penguin-bird-face-funny-cartoon-faces-s-face-smiley-bird.png'
+                },
+                { id: 4, name: 'Dima', logo: 'https://wallpaperaccess.com/full/777162.png' },
+                {
+                    id: 5,
+                    name: 'Sveta',
+                    logo: 'https://funnypicture.org/wallpaper/2015/05/funny-cartoon-faces-28-high-resolution-wallpaper.png'
+                }
+            ]
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: 'Sveta',
+                    logo: 'https://c0.klipartz.com/pngpicture/795/938/gratis-png-cerdo-blanco-grande-caras-divertidas-de-dibujos-animados-s.png'
+                },
+                {
+                    id: 2,
+                    name: 'Anna',
+                    logo: 'https://w7.pngwing.com/pngs/2/519/png-transparent-little-penguin-bird-face-funny-cartoon-faces-s-face-smiley-bird.png'
+                },
+                {
+                    id: 3,
+                    name: 'Dima',
+                    logo: 'https://funnypicture.org/wallpaper/2015/05/funny-cartoon-faces-28-high-resolution-wallpaper.png'
+                }
+            ]
+        }
+    },
 
+    _callSubscriber (state: stateType) {
+        console.log("State render")
+    },
+
+    subscribe(observer: (state: stateType) => void) {
+        this._callSubscriber = observer;
+    },
+
+    addPost ():void {
+        const newPost = {
+            id: 4,
+            message: this._state.profilePageData.newPostText,
+            likes: 0
+        };
+        this._state.profilePageData.postsData.push(newPost);
+        this._state.profilePageData.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText (newText: string): void {
+        this._state.profilePageData.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    getState(): stateType {
+        return this._state;
+    },
+}
