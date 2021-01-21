c09710057e6fbc42d1aa0795b4ac4b15b1e765b5
Yehor Pliasov <fenix4088@gmail.com>
2021-01-21 19:32:11 +0100

    54 added ComponentDidMount method to Users Comp

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -71,12 +71,15 @@ type UsersPropsType = {
 };*/
 
 export class Users extends React.Component<UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props);
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
             this.props.setUsers(response.data.items);
         });
+    }
+
+    componentDidUpdate() {
+        console.log("Did Update")
     }
 
     render() {
