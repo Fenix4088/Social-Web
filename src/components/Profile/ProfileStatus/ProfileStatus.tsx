04a397af64830541373f051927e5fb97e096cf79
Yehor Pliasov <fenix4088@gmail.com>
2021-03-05 19:08:59 +0100

    74 componentDidUpdate in ProfileStatus comp, fixed bug with loding status and profiel while in edit mode

--- src/components/Profile/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus/ProfileStatus.tsx
@@ -14,46 +14,62 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsT, ProfileS
     state: ProfileStatusLST = {
         editMode: false,
         status: this.props.status
-    }
+    };
 
     activateEditMode = () => {
         this.setState({
             editMode: true
-        })
-    }
+        });
+    };
 
     deactivateEditMode = () => {
         this.setState({
             editMode: false
-        })
+        });
         this.props.updateUserStatus(this.state.status);
-    }
+    };
 
     onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const {value} = e.currentTarget;
+        const { value } = e.currentTarget;
         this.setState({
             status: value
-        })
+        });
+    };
+
+    componentDidUpdate(
+        prevProps: Readonly<ProfileStatusPropsT>,
+        prevState: Readonly<ProfileStatusLST>
+    ) {
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+
+        }
     }
 
     render() {
-        console.log("props", this.props.status)
-        console.log("state", this.state.status)
-        const {status} = this.state;
+        console.log("props", this.props.status);
+        console.log("state", this.state.status);
+        const { status } = this.state;
 
         return (
             <div>
-                {!this.state.editMode ?
+                {!this.state.editMode ? (
                     <div>
                         <span onDoubleClick={this.activateEditMode}>{this.props.status || "No status"}</span>
                     </div>
-                    :
+                ) : (
                     <div>
-                        <input value={status} onBlur={this.deactivateEditMode} onChange={this.onStatusChange}
-                               autoFocus/>
+                        <input
+                            value={status}
+                            onBlur={this.deactivateEditMode}
+                            onChange={this.onStatusChange}
+                            autoFocus
+                        />
                     </div>
-                }
+                )}
             </div>
-        )
+        );
     }
 }
