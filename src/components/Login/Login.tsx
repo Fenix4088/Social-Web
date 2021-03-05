68e22caf9cd94e0337aecc3e8da0d4ae7f60b250
Yehor Pliasov <fenix4088@gmail.com>
2021-03-05 22:36:04 +0100

    75 Added redux-form libarary

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import LoginForm from "./LoginForm";
 
 export const Login = () => {
-    return <h1>Login</h1>
-}
+    const onSubmit = (formData: Object):void => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm onSubmit={onSubmit}/>
+        </div>
+    );
+};
+
